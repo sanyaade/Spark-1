60c4202630cc5f983ae119199a467c1bcfc1d7b1
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-02 13:33:19 +0100

    Got the validation script working

--- build/validate.js
+++ build/validate.js
@@ -1,22 +1,28 @@
-// Include fs
-var fs = require('fs');
+// Initialisation
+var src = require('fs').readFileSync('spark.js', 'utf8'),
+	sys = require('sys'),
+	jshint = require('./jshint').JSHINT,
+	i = null,
+	e = null;
 
-// Include jshint
-var jshint = require('./jshint').JSHINT;
-
-// Load the script
-fs.readFile('../spark.js', function(err, data) {
-	// Check for errors
-	if(err) {
-		// Throw the error
-		throw err;
-	}
-	else {
-		// Run jshint
-		var result = jshint(data, {
-			curly: true,
-			eqeqeq: true,
-			forin: true
-		});
-	}
+// Run jshint
+var result = jshint(src, {
+	curly: true,
+	eqeqeq: true,
+	forin: true,
+	browser: true
 });
+
+// Check for errors
+if(result) {
+	// All good baby, baby!
+	sys.puts('What beautiful syntax!');
+}
+else {
+	// It's the end of the world!
+	for(i = 0; i < jshint.errors.length; i++) {
+		// Log the error
+		e = jshint.errors[i];
+		sys.puts(e.raw + ' (' + e.line + ':' + e.character + ')' + "\n" + e.evidence + "\n");
+	}
+}
