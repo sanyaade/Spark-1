f9b529714c0df1146d954af8653ab3152170d20d
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-02 13:14:44 +0100

    Finished the validator script

--- build/validate.js
+++ build/validate.js
@@ -1,2 +1,22 @@
+// Include fs
+var fs = require('fs');
+
 // Include jshint
 var jshint = require('./jshint').JSHINT;
+
+// Load the script
+fs.readFile('../spark.js', function(err, data) {
+	// Check for errors
+	if(err) {
+		// Throw the error
+		throw err;
+	}
+	else {
+		// Run jshint
+		var result = jshint(data, {
+			curly: true,
+			eqeqeq: true,
+			forin: true
+		});
+	}
+});
