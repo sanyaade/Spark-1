3ada5c3d8af840d00388ed8cfa5eb9b29a386372
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-02 18:50:44 +0100

    Added the header comment to the core

---
+++ src/core.js
@@ -0,0 +1,8 @@
+/**
+ * @preserve Spark JavaScript Library v3.0.0
+ * http://sparkjs.co.uk/
+ * 
+ * Copyright 2011, Oliver Caldwell
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://sparkjs.co.uk/licence.html
+ */
