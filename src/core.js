4542620acf4c4ea8a0aaf2d939bc1305b6eff1dc
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-02 19:09:55 +0100

    Initialised and exposed the object

--- src/core.js
+++ src/core.js
@@ -7,5 +7,14 @@
  * http://sparkjs.co.uk/licence.html
  */
 (function() {
+	// Create the object
+	var Spark = {
+		
+	};
 	
+	// Check if the name is already in use
+	if(typeof window.Spark === 'undefined') {
+		// If not then expose the object
+		window.Spark = Spark;
+	}
 });
