01f2c36fec11593caaaea63355fb5f5080c59862
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-02 21:15:37 +0100

    Added the base content for the ready function

--- src/ready.js
+++ src/ready.js
@@ -2,8 +2,36 @@
  * Runs the specified function when the DOM is ready
  * 
  * @param {Function} fn Function to be run when the DOM is ready
- * @returns {this} For chaining
  */
 Spark.extend('ready', function(fn) {
-	
+	// Check if we can use addEventListener
+	if(window.addEventListener) {
+		// For all browsers except IE
+		document.addEventListener('DOMContentLoaded', function() {
+			fn();
+		}, false);
+	}
+	else {
+		// For IE
+		(function(){
+			// Check IE's proprietary DOM members
+			if(!document.uniqueID && document.expando) {
+				return;
+			}
+			
+			// Create the custom tag
+			var tempNode = document.createElement('document:ready');
+			
+			try {
+				// See if it throws errors until after it is ready
+				tempNode.doScroll('left');
+				
+				// Call ready
+				fn();
+			}
+			catch(err) {
+				setTimeout(arguments.callee, 0);
+			}
+		})();
+	}
 });
