1a6603430047f9b1dafe671a4d1a3726161490b3
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-03 19:36:52 +0100

    Looped through all parameters in the find function

--- src/find.js
+++ src/find.js
@@ -5,5 +5,16 @@
  * @returns {Object} Returns the Spark object to allow chaining
  */
 Spark.extend('find', function(parameters) {
+	// Initialise any required variables
+	var name = null,
+		value = null;
 	
+	// Loop through all the parameters
+	for(name in parameters) {
+		// Make sure it is not a prototype
+		if(parameters.hasOwnProperty(name)) {
+			// Grab the current parameter
+			value = parameters[name];
+		}
+	}
 });
