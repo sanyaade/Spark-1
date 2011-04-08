bd792b4738e23e50001c63584a094f728f550726
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-08 20:02:12 +0100

    Added the ability to pass custom data to the each function, it now takes objects or arrays. It also passes the current item into the this variable in the callback. I also changed the test to use the new method.

--- src/each.js
+++ src/each.js
@@ -2,16 +2,45 @@
  * Loops through all of the elmenets contained in the Spark object passing the to a function
  * 
  * @param {Function} fn Function for the current element to be passed to
+ * @param {Array|Object} data Optional data to be looped over, if not passed then it uses the elements
  * @returns {Object} Returns the Spark object for chaining
  */
-Spark.extend('each', function(fn) {
+Spark.extend('each', function(fn, data) {
 	// Initialise any required variables
-	var i = null;
+	var i = null,
+		target = null;
 	
-	// Loop through all elements
-	for(i = 0; i < this.length; i++) {
-		// Pass the element to the function
-		fn(this[i]);
+	// Check if there is any data
+	if(typeof data === 'undefined') {
+		// Theres no data, check for elements
+		if(typeof this.elements === 'undefined') {
+			// There is nothing to use, return false
+			return false;
+		}
+		else {
+			// There are some elelements, assign it to target
+			target = this.elements;
+		}
+	}
+	else if(typeof data === 'object') {
+		// We have data! Assign it to target
+		target = data;
+	}
+	
+	// Check if target is an array or object
+	if(target instanceof Array) {
+		// Loop through as an array
+		for(i = 0; i < target.length; i++) {
+			fn.apply(target[i]);
+		}
+	}
+	else {
+		// Loop through as an object
+		for(i in target) {
+			if(target.hasOwnProperty(i) === true) {
+				fn.apply(target[i]);
+			}
+		}
 	}
 	
 	// Return the Spark object for chaining
