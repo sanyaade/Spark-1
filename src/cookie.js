040ab1a8d05dc37dbaa6431cbd149d3f256a5883
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-09 21:00:30 +0100

    Documented the cookie function.

--- src/cookie.js
+++ src/cookie.js
@@ -1,10 +1,23 @@
 /**
- * Gets or sets cookies
+ * The cookie function is used to set or get cookies. To get the contents of a cookie simply call the function and provide the name of the cookie you wish to get the contents of. For example.
+ * 
+ *     // Returns the content of the cookie 'foo'
+ *     Spark.cookie('foo');
+ * 
+ * To set a cookie you just have to pass some content along with a name, like so.
+ * 
+ *     // Sets the cookie 'foo' to 'bar'
+ *     Spark.cookie('foo', 'bar');
+ * 
+ * You can also provide an optional duration which is how long the cookie should last for in milliseconds. If you do not provide a duration it will expire when the session ends. Say you wanted to set the previous cookie and make it last for five seconds, you would use the following line.
+ * 
+ *     // Sets a cookie that lasts five seconds
+ *     Spark.cookie('foo', 'bar', 5000);
  * 
  * @param {String} name The name of the cookie you wish to get or set
  * @param {String} content If passed the cookie will be set with this as it's content
- * @param {Number} duration The amount of miliseconds you wish the cookie to last for, if not set then it will last for the session
- * @returns {String} The content of the cookie whos name you specified
+ * @param {Number} duration The amount of milliseconds you wish the cookie to last for, if not set then it will last for the session
+ * @returns {String} The content of the cookie who's name you specified
  */
 Spark.extend('cookie', function(name, content, duration) {
 	// Initialise any required variables
