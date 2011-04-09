47a1e3d487dc39587d8ef18d76d31d3896cd9221
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-09 17:32:53 +0100

    Completely changed the data function, used the same code but now it is all within one function, also updated the tests.

--- src/data.js
+++ src/data.js
@@ -1,106 +1,60 @@
-Spark.extend('data', {
+Spark.extend('dataMeta', {
 	keys: [],
-	data: [],
+	data: []
+});
+
+Spark.extend('data', function(name, data) {
+	// Initialise any required variables
+	var i = null,
+		e = null,
+		target = null,
+		found = null;
 	
-	/**
-	 * Get the key id for the specified element
-	 * 
-	 * @param {Object} e The element you need to search for
-	 * @returns {Boolean|Number} It will return false if not found or the key ID if successful
-	 */
-	search: function(e) {
-		// Initialise any required variables
-		var i = null;
-		
-		// Loop through all of the keys checking for our element
-		for(i = 0; i < this.keys.length; i++) {
-			if(this.keys[i] === e) {
-				// Found it, return the location
-				return i;
+	// Check that we have an element to work with
+	if(this.elements instanceof Array) {
+		if(typeof this.elements[0] === 'object') {
+			// Grab the element
+			e = this.elements[0];
+			
+			// Start the target as false
+			target = false;
+			
+			// Loop through all of the keys checking for our element
+			for(i = 0; i < this.dataMeta.keys.length; i++) {
+				if(this.dataMeta.keys[i] === e) {
+					// Found it, assign it to the target
+					target = i;
+				}
 			}
-		}
-		
-		// Default to returning false
-		return false;
-	},
-	
-	/**
-	 * Assigns data to the elements located in the Spark instance
-	 * 
-	 * @param {String} name The name you wish to assign your data under
-	 * @param {Mixed} data The variable you wish to assign, it can be anything
-	 */
-	set: function(name, data) {
-		// Initialise any required variables
-		var i = null,
-			e = null,
-			target = null;
-		
-		// Check that we have some elements to work with
-		if(this.elements instanceof Array) {
-			// Loop through all of the elements grabbing the current one
-			for(i = 0; i < this.length; i++) {
-				e = this[i];
-				
-				// Search for the element
-				target = this.search(e);
-				
-				// Check if we have not found our element
+			
+			// Check if data was passed
+			if(typeof data !== 'undefined') {
+				// We have data
+				// Check if we found an element
 				if(target === false) {
-					// Set the target to the new location
-					target = this.keys.length;
-					
-					// Add the element to the keys
-					this.keys.push(e);
+					// We did not, create it and set the target
+					target = this.dataMeta.keys.length;
+					this.dataMeta.keys.push(e);
+					this.dataMeta.data.push({});
 				}
 				
 				// Assign the data
-				this.data[target][name] = data;
+				this.dataMeta.data[target][name] = data;
 			}
-		}
-		
-		// Return the Spark object for chaining
-		return this;
-	},
-	
-	/**
-	 * Retrieve data from the elements located in the Spark instance
-	 * 
-	 * @param {String} name The name of the data you wish to retrieve
-	 * @param {Function} fn A function you wish the retrieved data to be passed to, if not found it will pass false
-	 */
-	get: function(name, fn) {
-		// Initialise any required variables
-		var i = null,
-			e = null,
-			target = null,
-			found = null;
-		
-		// Check that we have some elements to work with
-		if(this.elements instanceof Array) {
-			// Loop through all of the elements grabbing the current one
-			for(i = 0; i < this.length; i++) {
-				e = this[i];
-				
-				// Search for the element
-				target = this.search(e);
-				
-				// Check if we have not found our element in the keys
+			else {
+				// We do not have data
+				// Check if we found an element
 				if(target === false) {
-					// Prepare to pass false back to the callback
-					found = false;
-				}
-				else {
-					// We found it, so prepare to pass the found data to the callback
-					found = (this.data[target][name]) ? this.data[target][name] : false;
+					// We did not, return false
+					return false;
 				}
 				
-				// Pass what we found to the callback
-				fn(found);
+				// We now return the found data
+				return this.dataMeta.data[target][name];
 			}
 		}
-		
-		// Return the Spark object for chaining
-		return this;
 	}
+	
+	// Return false bacause there is nothing to use
+	return false;
 });
