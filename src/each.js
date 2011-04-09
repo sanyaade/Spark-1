4236ced0f65663faf81d0e10e5cb48dd0eb0af42
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-09 21:22:18 +0100

    Documented the each function

--- src/each.js
+++ src/each.js
@@ -1,5 +1,20 @@
 /**
- * Loops through all of the elmenets contained in the Spark object passing the to a function
+ * Loops through all of the elements contained in the Spark object passing them to a specified callback function.
+ * You can also pass an array or object as the second argument and it will loop through that instead.
+ * 
+ * This basically handles looping for you, if you pass it an object then it will perform a `hasOwnProperty` check before passing it to your callback function.
+ * 
+ * To loop through all of the p tags on the page and turn them red, you would use the following code.
+ * 
+ *     $('p').each(function(e) {
+ *         e.style.color = '#FF0000';
+ *     });
+ * 
+ * To loop through an array or object you just pass the variable in question to the each function as the second argument. Here is an example of passing an array.
+ * 
+ *     Spark.each(function(e) {
+ *         console.log(e);
+ *     }, ['hello', 'world']);
  * 
  * @param {Function} fn Function for the current element to be passed to
  * @param {Array|Object} data Optional data to be looped over, if not passed then it uses the elements
