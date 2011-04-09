8cf40e299d0afa4af05f2ba7b85bf49883f4b8ed
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-09 16:15:27 +0100

    Added the JSDoc and shuffled the object about a bit.

--- src/data.js
+++ src/data.js
@@ -1,10 +1,23 @@
 Spark.extend('data', {
+	keys: [],
+	data: [],
+	
+	/**
+	 * Assigns data to the elements located in the Spark instance
+	 * 
+	 * @param {String} name The name you wish to assign your data under
+	 * @param {Mixed} data The variable you wish to assign, it can be anything
+	 */
 	set: function(name, data) {
 		
 	},
+	
+	/**
+	 * Retrieve data from the elements located in the Spark instance
+	 * 
+	 * @param {String} name The name of the data you wish to retrieve
+	 */
 	get: function(name) {
 		
-	},
-	keys: [],
-	data: []
+	}
 });
