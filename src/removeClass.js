5fb1080c93ad0bb05ff8a68a98f17e5badb12875
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-12 19:04:28 +0100

    Added the code for the removeClass function

---
+++ src/removeClass.js
@@ -0,0 +1,20 @@
+/**
+ * Remove a class from the specified elements
+ * 
+ * This will remove the class, border, from all the h1 tags on the page.
+ * 
+ *     $('h1').removeClass('border');
+ * 
+ * @param {String} name The class name you want to remove
+ * @returns {Object} Returns the Spark object for chaining
+ */
+Spark.extend('removeClass', function(name) {
+	// Loop through all the elements
+	this.each(function(e) {
+		// Remove the class
+		$(e).attribute('class', $(e).attribute('class').replace(new RegExp('(^|\\s)' + name + '($|\\s)'), ''));
+	});
+	
+	// Return the Spark object for chaining
+	return this;
+});
