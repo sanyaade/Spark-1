0d011230e19c6082d10d8911657a2daa4886c912
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-12 13:24:17 +0100

    Gutted the classes function

--- src/classes.js
+++ src/classes.js
@@ -2,22 +2,5 @@
  * Adds or removes classes from elements
  */
 Spark.extend('classes', function() {
-	// Initialise the object
-	var built = {
-		add: function(name) {
-			// Return the Spark object for chaining
-			return this.that;
-		},
-		
-		remove: function(name) {
-			// Return the Spark object for chaining
-			return this.that;
-		}
-	};
 	
-	// Add the reference to the Spark object
-	built.that = this;
-	
-	// Return the object
-	return built;
 });
