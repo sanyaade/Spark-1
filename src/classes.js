6a8b4b0f053b670f8cbbe16531898a4ccace53b3
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-12 11:45:24 +0100

    Added the basic structure of the classes function

---
+++ src/classes.js
@@ -0,0 +1,21 @@
+/**
+ * Adds or removes classes from elements
+ */
+Spark.extend('classes', function() {
+	// Initialise the object
+	var built = {
+		add: function(name) {
+			
+		},
+		
+		remove: function(name) {
+			
+		}
+	};
+	
+	// Add the reference to the Spark object
+	built.that = this;
+	
+	// Return the object
+	return built;
+});
