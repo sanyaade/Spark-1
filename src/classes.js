a68ebb6861179528c9efaa69da74b76d2cf849ed
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-12 13:38:38 +0100

    Extended the extend function to allow calling and got the basic version of the classes function working.

--- src/classes.js
+++ src/classes.js
@@ -2,5 +2,20 @@
  * Adds or removes classes from elements
  */
 Spark.extend('classes', function() {
+	// Set up the functions
+	var built = {
+		add: function() {
+			return this.that;
+		},
+		
+		remove: function() {
+			return this.that;
+		}
+	};
 	
-});
+	// Put this in scope
+	built.that = this;
+	
+	// Return the object
+	return built;
+}, true);
