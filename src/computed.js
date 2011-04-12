c36180b1c1f6a1d0ae45d2e14588a0b2a2e72dc8
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-12 11:09:04 +0100

    Added the code and JSDoc for the computed function

---
+++ src/computed.js
@@ -0,0 +1,20 @@
+/**
+ * Retrieves the specified computed style from the first element in the list.
+ * 
+ * If you wanted to get the computed height of a specified div you would use the following line.
+ * 
+ *     $('div#chat').computed('height');
+ * 
+ * @param {String} name The name of the computed style you require
+ * @returns {String} The computed style of the first element in the element list
+ */
+Spark.extend('computed', function(name) {
+	// If we can use getComputedStyle
+	if(getComputedStyle) {
+		// Return getComputedStyle
+		return getComputedStyle(this[0], null)[name];
+	}
+	
+	// Otherwise return currentStyle
+	return this[0].currentStyle[name];
+});
