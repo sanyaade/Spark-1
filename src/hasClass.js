a77f5c186ea6de6e28a4d1c90c42187a3a4f2fbb
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-12 14:47:59 +0100

    Finished the hasClass function

--- src/hasClass.js
+++ src/hasClass.js
@@ -1,7 +1,14 @@
 /**
- * Checks if an element has the specified class
+ * Checks if an element has the specified class.
+ * 
+ * For example, this will return true, given that there is a div with a class of focus on the page.
+ * 
+ *     $('div.focus').hasClass('focus');
+ * 
+ * @param {String} name The class name you want to search for
+ * @returns {Boolean} Will return true if it has the class or false if it does not
  */
 Spark.extend('hasClass', function(name) {
-	// Return the Spark object for chaining
-	return this;
+	// Check for the class
+	return new RegExp('\\b' + name + '\\b').test(this.attribute('class'));
 });
