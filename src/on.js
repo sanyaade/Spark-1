51db0cebb189a2851475b0c198b6ecbc25d8e494
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-14 20:35:45 +0100

    Added the stopBubble argument and added the fixEvent function

--- src/on.js
+++ src/on.js
@@ -3,9 +3,53 @@
  * 
  * @param {String} name Name of the event you want to listen for
  * @param {Function} fn Function to be run when the event occurs
+ * @param {Boolean} stopBubble If true then event bubbling will be prevented
  */
-Spark.extend('on', function(name, fn) {
-	// Initialise any required variables
+Spark.extend('on', function(name, fn, stopBubble) {
+	function fixEvent(e) {
+		// Initalise any required variables
+		var posx = 0,
+			posy = 0,
+			obj = null,
+			offsetX = 0,
+			offsetY = 0;
+		
+		// Fix IE's wrong association of the target element
+		if(e.srcElement) {
+			e.target = e.srcElement;
+		}
+		
+		// Fix Safaris problem with selecting the wrong node type
+		if(targ.nodeType == 3) {
+			targ = targ.parentNode;
+		}
+		
+		// Make sure we have keyCode, and not which
+		if(e.which) {
+			e.keyCode = e.which;
+		}
+		
+		// Fix IE's pageX/Y locations
+		if(e.clientX || e.clientY) {
+			e.pageX = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			e.pageY = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+		}
+		
+		// Fix the offsetX/Y in Firefox
+		obj = e.target;
+		if(obj.offsetParent) {
+			do {
+				offsetX += obj.offsetLeft;
+				offsetY += obj.offsetTop;
+			} while(obj = obj.offsetParent);
+			
+			e.offsetX = offsetX;
+			e.offsetY = offsetY;
+		}
+		
+		// Return the fixed event object
+		return e;
+	}
 	
 	// Loop through all the elements
 	this.each(function(e) {
