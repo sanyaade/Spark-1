ddfe88f86cec0bf6bca284d86303e1cf5094d975
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-14 13:22:21 +0100

    Added the basic structure for the on function

---
+++ src/on.js
@@ -0,0 +1,12 @@
+/**
+ * Add an event listener to the found elements
+ * 
+ * @param {String} name Name of the event you want to listen for
+ * @param {Function} fn Function to be run when the event occurs
+ */
+Spark.extend('on', function(name, fn) {
+	// Initialise any required variables
+	
+	// Return the Spark object for chaining
+	return this;
+});
