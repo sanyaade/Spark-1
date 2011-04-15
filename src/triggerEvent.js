43ed8db8f99dbd623258d5b316bb55d453a9c66c
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-15 18:49:25 +0100

    Added the code for the triggerEvent function

---
+++ src/triggerEvent.js
@@ -0,0 +1,26 @@
+/**
+ * Manually fire events assigned to elements
+ * 
+ * @param {String} type Name of the event you wish to trigger
+ * @returns {Object} An instance of Spark to allow chaining
+ */
+Spark.extend('triggerEvent', function(type) {
+	// Loop through all of the elements
+	this.each(function(e) {
+		// Check for createEventObject
+		if(document.createEventObject) {
+			// Trigger for Internet Explorer
+			trigger = document.createEventObject();
+			e.fireEvent('on' + type, trigger);
+		}
+		else {
+			// Trigger for the good browsers
+			trigger = document.createEvent('HTMLEvents');
+			trigger.initEvent(type, true, true);
+			e.dispatchEvent(trigger);
+		}
+	});
+	
+	// Return the Spark object for chaining
+	return this;
+});
