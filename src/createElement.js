5c114ce66fd16149958eac491217878971b13dc3
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-15 20:35:42 +0100

    Added the basic code for the createElement function, this consists of a JSDoc and the call to Spark.extend

---
+++ src/createElement.js
@@ -0,0 +1,11 @@
+/**
+ * Creates an element from the passed parameters
+ * 
+ * @param {String} name Name of the node you wish to create
+ * @param {Object} attributes Attributes to assign to the element
+ * @param {Object} styles Styles to assign to the element
+ * @returns {Object} The created element
+ */
+Spark.extend('createElement', function(name, attributes, styles) {
+	
+});
