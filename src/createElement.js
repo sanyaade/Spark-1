18db42c980b9e1ea12a395c058489acf7e12b432
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-15 20:47:23 +0100

    Added documentation for the createElement function.

--- src/createElement.js
+++ src/createElement.js
@@ -1,6 +1,28 @@
 /**
  * Creates an element from the passed parameters
  * 
+ * To create a basic element such as a p tag, all you need to provide is a tag name.
+ * 
+ *     Spark.createElement('p');
+ * 
+ * And the new p tag object will be returned.
+ * 
+ * If you wanted to assign a class and title to this element, you pass the attribute assigning object as the second argument.
+ * 
+ *     Spark.createElement('p', {
+ *         class: 'someClass',
+ *         title: 'Some title'
+ *     });
+ * 
+ * You can also style the element with the third argument.
+ * 
+ *     Spark.createElement('p', {
+ *         class: 'someClass',
+ *         title: 'Some title'
+ *     }, {
+ *         'background-color': '#FF0000'
+ *     });
+ * 
  * @param {String} name Name of the node you wish to create
  * @param {Object} attributes Attributes to assign to the element
  * @param {Object} styles Styles to assign to the element
