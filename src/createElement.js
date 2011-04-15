4f31ff70dbadd302f03bce251dca74523d8016a9
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-15 20:41:00 +0100

    Finished the code for the createElement function

--- src/createElement.js
+++ src/createElement.js
@@ -7,5 +7,19 @@
  * @returns {Object} The created element
  */
 Spark.extend('createElement', function(name, attributes, styles) {
+	// Create the new element
+	var built = document.createElement(name);
 	
+	// Assign the attributes if required
+	if(attributes) {
+		this.find(built).attribute(attributes);
+	}
+	
+	// Assign the styles if required
+	if(styles) {
+		this.find(built).style(styles);
+	}
+	
+	// Return the built element
+	return built;
 });
