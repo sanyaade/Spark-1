6209edcd1e49ecc61058ddd0ce57a79bb5a7b80a
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-18 14:41:17 +0100

    Added JSDoc for the load function

--- src/load.js
+++ src/load.js
@@ -1,5 +1,11 @@
 /**
  * Load external JavaScript files into the document
+ * 
+ * For example, to load a Spark plugin into the document you would use the following line.
+ * 
+ *     Spark.load('http://somesite/AwesomeSparkPlugin.js');
+ * 
+ * @param {String} file Path to the script you want to load
  */
 Spark.extend('load', function(file) {
 	// Add the script tag
