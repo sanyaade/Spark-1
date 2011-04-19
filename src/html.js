b314a1d99e4cccfff0f044bd36952bad32980b34
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-19 12:36:12 +0100

    Added the getting of inner html

--- src/html.js
+++ src/html.js
@@ -2,6 +2,11 @@
  * Replace or append a html string to the specified elements
  */
 Spark.extend('html', function(content, append) {
+	// Check if they just want the innerHTML
+	if(typeof content === 'undefined') {
+		return this[0].innerHTML;
+	}
+	
 	// Loop through all the elements
 	this.each(function(e) {
 		// Append or replace the html
