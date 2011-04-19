0dc1a9dd50f60a3ce48521afe2d9351672b71980
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-19 12:30:43 +0100

    Added the code for the html function

--- src/html.js
+++ src/html.js
@@ -2,5 +2,17 @@
  * Replace or append a html string to the specified elements
  */
 Spark.extend('html', function(content, append) {
+	// Loop through all the elements
+	this.each(function(e) {
+		// Append or replace the html
+		if(append) {
+			e.innerHTML += content;
+		}
+		else {
+			e.innerHTML = content;
+		}
+	});
 	
+	// Return the Spark object for chaining
+	return this;
 });
