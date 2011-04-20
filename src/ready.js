b27ae94ca23f445a85c231b806398a2b31f960ae
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-20 20:46:29 +0100

    Changed the ready function so now it checks if the ready has already been called.

--- src/ready.js
+++ src/ready.js
@@ -9,27 +9,43 @@
  * @param {Function} fn Function to be run when the DOM is ready
  */
 Spark.extend('ready', function(fn) {
-	// Check if we can use addEventListener
-	if(window.addEventListener) {
-		// For all browsers except IE
-		document.addEventListener('DOMContentLoaded', fn, false);
+	// Initialise any required variables
+	var that = this.clone();
+	
+	/**
+	 * Runs the call back and sets the already run flag to true
+	 */
+	function ready() {
+		that.find(document).data('domReady', true);
+		fn();
+	}
+	
+	if(!that.find(document).data('domReady')) {
+		// Check if we can use addEventListener
+		if(window.addEventListener) {
+			// For all browsers except IE
+			document.addEventListener('DOMContentLoaded', ready, false);
+		}
+		else {
+			// For IE
+			(function() {
+				// Create the custom tag
+				var tempNode = document.createElement('document:ready');
+
+				try {
+					// See if it throws errors until after it is ready
+					tempNode.doScroll('left');
+
+					// Call the function
+					ready();
+				}
+				catch(err) {
+					setTimeout(arguments.callee, 0);
+				}
+			}());
+		}
 	}
 	else {
-		// For IE
-		(function() {
-			// Create the custom tag
-			var tempNode = document.createElement('document:ready');
-			
-			try {
-				// See if it throws errors until after it is ready
-				tempNode.doScroll('left');
-				
-				// Call the function
-				fn();
-			}
-			catch(err) {
-				setTimeout(arguments.callee, 0);
-			}
-		})();
+		fn();
 	}
 });
