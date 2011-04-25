32db76e1b25946ebcbd6bf9b5606a78d921f572e
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-25 21:04:37 +0100

    Added the basic structure for the animate function

--- src/animate.js
+++ src/animate.js
@@ -1,6 +1,19 @@
 /**
  * Animate styles of the specified elements
  */
-Spark.extend('animate', function() {
+Spark.extend('animate', function(style, target) {
+	// Initialise any required variables
+	var that = this;
 	
+	// Check if we need to perform multiple animations
+	if(typeof style === 'object') {
+		// Loop over the animations calling them
+		this.each(function(t, s) {
+			that.animate(s, t);
+		}, style);
+		
+		return this;
+	}
+	
+	return this;
 });
