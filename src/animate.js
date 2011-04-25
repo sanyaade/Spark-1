224872e5fe4584ff9aa25a28322dedf83ec34e67
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-25 22:25:54 +0100

    Renamed target to "to"

--- src/animate.js
+++ src/animate.js
@@ -2,10 +2,10 @@
  * Animate styles of the specified elements
  * 
  * @param {String|Object} style Name of the style you wish to animate
- * @param {String|Number} target Only required if style is a string, the target to animate to. If it is a number it will append px as the unit
+ * @param {String|Number} to Only required if style is a string, the target to animate to. If it is a number it will append px as the unit
  * @returns {Object} Returns the Spark object for chaining
  */
-Spark.extend('animate', function(style, target) {
+Spark.extend('animate', function(style, to) {
 	// Initialise any required variables
 	var that = this,
 		from = null,
@@ -24,17 +24,17 @@ Spark.extend('animate', function(style, target) {
 			// Grab where we need to animate from
 			from = that.find(e).style(style);
 			
-			// Get the unit if the target is a string
-			if(typeof target === 'string') {
-				unit = target.replace(/[^%|in|cm|mm|em|ex|pt|pc|px]/gi, '');
+			// Get the unit if the to is a string
+			if(typeof to === 'string') {
+				unit = to.replace(/[^%|in|cm|mm|em|ex|pt|pc|px]/gi, '');
 			}
 			else {
 				// Otherwise set it to an empty string
 				unit = '';
 			}
 			
-			// Turn the target and from into an integer
-			target = parseFloat(target);
+			// Turn the to and from into an integer
+			to = parseFloat(to);
 			from = parseFloat(from);
 		});
 	}
