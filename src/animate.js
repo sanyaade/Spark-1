a6b38075253df92f09ec98e5309687ce166810d7
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-25 21:10:45 +0100

    Added JSDoc for the animate function arguments

--- src/animate.js
+++ src/animate.js
@@ -1,5 +1,9 @@
 /**
  * Animate styles of the specified elements
+ * 
+ * @param {String|Object} style Name of the style you wish to animate
+ * @param {String|Number} target Only required if style is a string, the target to animate to. If it is a number it will append px as the unit
+ * @returns {Object} Returns the Spark object for chaining
  */
 Spark.extend('animate', function(style, target) {
 	// Initialise any required variables
