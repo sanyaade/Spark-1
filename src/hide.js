813445e9d241fb7f98108a0144f55a5ab0a958d6
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-26 11:16:07 +0100

    Made the show and hide functions take the original display into account and fixed a scope problem.

--- src/hide.js
+++ src/hide.js
@@ -2,5 +2,15 @@
  * Hides all of the found elements
  */
 Spark.extend('hide', function() {
+	// Initialise any required variables
+	var that = this.clone();
+	
+	// Loop through all the elements
+	this.each(function(e) {
+		// Store the original display type
+		that.find(e).data('SparkDisplayType', that.find(e).style('display'));
+	});
+	
+	// Hide the element
 	this.style('display', 'none');
 });
--- src/show.js
+++ src/show.js
@@ -2,5 +2,12 @@
  * Shows all of the found elements
  */
 Spark.extend('show', function() {
-	this.style('display', 'block');
+	// Initialise any required variables
+	var that = this.clone();
+	
+	// Loop through all the elements
+	this.each(function(e) {
+		// Show the element with the correct display type
+		that.find(e).style('display', that.find(e).data('SparkDisplayType'));
+	});
 });
