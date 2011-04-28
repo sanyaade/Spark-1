c3d69ea94c3a1dd60511adc588876f622fa3673d
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-28 12:37:54 +0100

    Added the basic structure for the color function

---
+++ src/color.js
@@ -0,0 +1,36 @@
+/**
+ * Converts colors to RGB or hex from RGB, hex or the color name
+ */
+
+/** @private */
+Spark.extend('color', {
+    /**
+     * Converts any color to an object containing its rgb values
+     * 
+     * @param {String|Object} color The color string to convert (RGB, hex or a color name such as 'red') or the RGB object
+     * @returns {Object} The object containing the rgb values
+     */
+	toObject: function(color) {
+		
+	},
+	
+    /**
+     * Converts any color to a RGB string
+     * 
+     * @param {String|Object} color The color string to convert (RGB, hex or a color name such as 'red') or the RGB object
+     * @returns {String} The RGB string
+     */
+	toRgb: function(color) {
+		
+	},
+	
+    /**
+     * Converts any color to a hex string
+     * 
+     * @param {String|Object} color The color string to convert (RGB, hex or a color name such as 'red') or the RGB object
+     * @returns {String} The hex string
+     */
+	toHex: function(color) {
+		
+	}
+});
