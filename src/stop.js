966fc2e688db33ec4d42d4516e2bd0cc96eaf27f
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-29 19:07:35 +0100

    Added the contents of the stop function

--- src/stop.js
+++ src/stop.js
@@ -2,5 +2,22 @@
  * Stops all animations for the specified elements
  */
 Spark.extend('stop', function() {
+	// Initialise any required variables
+	var i = null,
+		timeouts = null,
+		that = this;
 	
+	// Loop through all of the elements
+	this.each(function(e) {
+		// Grab the timesouts
+		timeouts = that.find(e).data('SparkTimeouts');
+		
+		// Loop through all of the timeouts
+		for(i = 0; i < timeouts.length; i++) {
+			clearTimeout(timeouts[i]);
+		}
+		
+		// Reset the array
+		that.find(e).data('SparkTimeouts', []);
+	});
 });
