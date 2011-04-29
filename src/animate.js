f3fe4169d2313a318f4389ebdb552da67e9c0973
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-29 16:07:36 +0100

    Removed the use of the noCallback flag. Instead it just does not pass the callback, duh!

--- src/animate.js
+++ src/animate.js
@@ -289,10 +289,9 @@ Spark.extend('animate', function(animations, timeframe, easing, callback) {
 		}, time);
 	}
 	
-	function stackAnimation(e, animations, timeframe, easing, callback) {
+	function stackAnimation(e, animations, timeframe, easing) {
 		setTimeout(function() {
-			e.noCallback = true;
-			e.animate(animations, timeframe, easing, callback);
+			e.animate(animations, timeframe, easing);
 		}, e.data('SparkOffset'));
 	}
 	
@@ -325,7 +324,7 @@ Spark.extend('animate', function(animations, timeframe, easing, callback) {
 		// Check if we can call now, or if we need to add it to the animation stack
 		if(found.data('SparkOffset') > 0) {
 			// Add it to the stack
-			stackAnimation(found, animations, timeframe, easing, callback);
+			stackAnimation(found, animations, timeframe, easing);
 			
 			// Return out of the loop
 			return false;
@@ -404,7 +403,7 @@ Spark.extend('animate', function(animations, timeframe, easing, callback) {
 	});
 	
 	// Set the callback to be run if one was passed
-	if(typeof callback === 'function' && this.noCallback !== true) {
+	if(typeof callback === 'function') {
 		setTimeout(function() {
 			// Loop through all the elements
 			that.each(function(e) {
