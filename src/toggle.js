3560363cfe46f485eeca19be1371678300cce9b8
Oliver Caldwell <oliver@flowdev.co.uk>
2011-04-29 19:18:32 +0100

    Added the contents of the toggle function

--- src/toggle.js
+++ src/toggle.js
@@ -2,5 +2,26 @@
  * Toggle the visibility of the specified elements
  */
 Spark.extend('toggle', function() {
+	// Initialise any required variables
+	var that = this,
+		element = null;
 	
+	// Loop through the elements
+	this.each(function(e) {
+		// Grab the element
+		element = that.find(e);
+		
+		// Check wether it is display none or not
+		if(element.style('display') === 'none') {
+			// It is, show it
+			element.show();
+		}
+		else {
+			// It is not, hide it
+			element.hide();
+		}
+	});
+	
+	// Return the Spark object
+	return this;
 });
