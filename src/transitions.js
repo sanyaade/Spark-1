21ae159bde1316b2a4474064e353f2affdad5a1e
Oliver Caldwell <oliver@flowdev.co.uk>
2011-05-02 15:30:06 +0100

    Added the contents of the fade transition

--- src/transitions.js
+++ src/transitions.js
@@ -4,7 +4,10 @@
 Spark.extend('transitions', {
 	show: {
 		fade: function(element) {
-			
+			// Show the element, set its opacity to 0 and fade its opacity to 1
+			element.show().style('opacity', 0).animate({
+				opacity: 1
+			});
 		}
 	},
 	hide: {
