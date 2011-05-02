37fd21bf654f5b7dc1e92bb9b8a61ff50558b602
Oliver Caldwell <oliver@flowdev.co.uk>
2011-05-02 15:51:27 +0100

    Added the transition running code to the hide function

--- src/hide.js
+++ src/hide.js
@@ -9,18 +9,36 @@
  * 
  * @returns {Object} Returns the Spark object for chaining
  */
-Spark.extend('hide', function() {
+Spark.extend('hide', function(transition) {
 	// Initialise any required variables
 	var that = this;
 	
-	// Loop through all the elements
-	this.each(function(e) {
-		// Make sure it is already shown
-		if(that.find(e).style('display') !== 'none') {
-			// Store the original display type and hide the element
-			that.find(e).data('SparkDisplayType', that.find(e).style('display')).style('display', 'none');
+	// Check for a transition
+	if(typeof transition === 'string') {
+		// Hide it with the specified transition
+		// Check for the transition
+		if(typeof this.transitions.hide[transition] === 'function') {
+			// Loop through all of the elements
+			this.each(function(e) {
+				// Run the transition
+				that.transitions.hide[transition](that.find(e));
+			});
 		}
-	});
+		else {
+			// If it does not exist, default to just hiding it
+			this.hide();
+		}
+	}
+	else {
+		// Loop through all the elements
+		this.each(function(e) {
+			// Make sure it is already shown
+			if(that.find(e).style('display') !== 'none') {
+				// Store the original display type and hide the element
+				that.find(e).data('SparkDisplayType', that.find(e).style('display')).style('display', 'none');
+			}
+		});
+	}
 	
 	// Return the Spark object for chaining
 	return this;
