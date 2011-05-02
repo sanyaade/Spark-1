0680d5a4d314743e80a27e8640979b2ffef7328c
Oliver Caldwell <oliver@flowdev.co.uk>
2011-05-02 15:49:23 +0100

    Added the smooth transition

--- src/transitions.js
+++ src/transitions.js
@@ -27,6 +27,30 @@ Spark.extend('transitions', {
 			}, false, false, function() {
 				element.style('overflow', originalOverflow);
 			});
+		},
+		smooth: function(element) {
+			// Show the element and grab its height
+			var originalHeight = element.show().style('height'),
+				// Grab its width
+				originalWidth = element.style('width'),
+				// Grab its opacity
+				originalOpacity = element.style('opacity'),
+				// Grab its overflow and default to visible
+				originalOverflow = element.style('overflow') || 'visible';
+			
+			// Set the height, width and opacity to 0. Set the overflow to hidden and then animate everything to its original values. Finally, set the overflow to its original value
+			element.style({
+				height: 0,
+				width: 0,
+				opacity: 0,
+				overflow: 'hidden'
+			}).animate({
+				height: originalHeight,
+				width: originalWidth,
+				opacity: originalOpacity
+			}, false, false, function() {
+				element.style('overflow', originalOverflow);
+			});
 		}
 	},
 	hide: {
