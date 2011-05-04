9e3bc72252bb61d35a0e686f40b53b5d6c7af39d
Oliver Caldwell <oliver@flowdev.co.uk>
2011-05-04 21:19:04 +0100

    Updated the JSDoc for the toggle function

--- src/toggle.js
+++ src/toggle.js
@@ -5,6 +5,20 @@
  * 
  *     $('p').toggle();
  * 
+ * To fade the elements in, you would specify the fade transition as the optional argument
+ * 
+ *     $('p').show('fade');
+ * 
+ * You can also pass a function as the second argument to be run when the transition completes, like so
+ * 
+ *     $('p').toggle('fade', function() {
+ *         alert('done');
+ *     });
+ * 
+ * This function takes the originial display type into account.
+ * 
+ * @param {String} transition Optional name of the transition to use to toggle. Default transitions are: fade, slide and smooth
+ * @param {Function} callback Optional function to be run after the transition completes
  * @returns {Object} Returns the Spark object for chaining
  */
 Spark.extend('toggle', function() {
