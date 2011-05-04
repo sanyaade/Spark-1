3c2f4d3e400c8a79fb44135ee69720ce62dc7dfe
Oliver Caldwell <oliver@flowdev.co.uk>
2011-05-04 22:06:57 +0100

    Added the ability to pass arrays to removeClass

--- src/removeClass.js
+++ src/removeClass.js
@@ -9,10 +9,23 @@
  * @returns {Object} Returns the Spark object for chaining
  */
 Spark.extend('removeClass', function(name) {
+	// Put this in scope
+	var that = this;
+	
 	// Loop through all the elements
 	this.each(function(e) {
-		// Remove the class
-		e.className = e.className.replace(new RegExp('(^|\\s)' + name + '($|\\s)'), '');
+		// Check if name is an array
+		if(name instanceof Array) {
+			// Loop over the array
+			that.each(function(n) {
+				// Remove the class
+				e.className = e.className.replace(new RegExp('(^|\\s)' + n + '($|\\s)'), '');
+			}, name);
+		}
+		else {
+			// Remove the class
+			e.className = e.className.replace(new RegExp('(^|\\s)' + name + '($|\\s)'), '');
+		}
 	});
 	
 	// Return the Spark object for chaining
